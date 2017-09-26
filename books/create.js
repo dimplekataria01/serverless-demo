849c662adc19c91a8a8db6c1c2939186c7751ab0
Kataria, Dimple <dimple.kataria@accenture.com>
2017-09-26 12:51:43 -0400

    project for aws serverless demo

--- books/create.js
+++ books/create.js
@@ -4,12 +4,14 @@ const AWS = require('aws-sdk');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
+console.log('Creating new books for you everyday');
+
 module.exports.create = (event, context, callback) => {
   const timestamp = new Date().getTime();
   const data = JSON.parse(event.body);
 
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
+    TableName: 'booksTable',
     Item: {
       id: uuid.v4(),
       title: data.title,
@@ -22,6 +24,13 @@ module.exports.create = (event, context, callback) => {
 
   // write the entry to the database
   dynamoDb.put(params, (error) => {
+
+   if (typeof data.title !== 'string') {
+      console.error('Validation Failed');
+      callback(new Error('Couldn\'t submit book because of validation errors.'));
+      return;
+    }
+
     // handle potential errors
     if (error) {
       console.error(error);
@@ -37,3 +46,15 @@ module.exports.create = (event, context, callback) => {
     callback(null, response);
   });
 };
+
+module.exports.dynamoDBStreamTriggered = (event, context, callback) => {
+ console.log('event::dynamoDBStreamTriggered ::'+JSON.stringify(event.Records));
+
+ const records = JSON.stringify(event.Records);
+  var key = records.dynamodb.Keys.id;
+  console.log('key::dynamoDBStreamTriggered ::'+key);
+  callback(null, { message: 'Dynamodb item was modified with key: '+ key + '!', event });
+};
+
+
+
--- books/list.js
+++ books/list.js
@@ -4,19 +4,19 @@ const AWS = require('aws-sdk');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const params = {
-  TableName: process.env.DYNAMODB_TABLE,
+  TableName: 'booksTable',
 };
 
 module.exports.list = (event, context, callback) => {
   // fetch all books from the database
   dynamoDb.scan(params, (error, result) => {
+
     // handle potential errors
     if (error) {
       console.error(error);
       callback(new Error('Couldn\'t fetch the books.'));
       return;
     }
-
     // create a response
     const response = {
       statusCode: 200,
